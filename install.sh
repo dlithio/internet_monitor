bd5db5805e15c4429643bcf5dada56d05b86fc7b
dlithio <dlithio@users.noreply.github.com>
2018-07-04 20:19:44 -0500

    Fix ownership of install directory

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo adduser --disabled-login inet_mon
 sudo cp -r $DIR/../internet_monitor /home/inet_mon
+sudo chown -R inet_mon:inet_mon /home/inet_mon
 sudo crontab -u inet_mon $DIR/cron/user.crontab
 
